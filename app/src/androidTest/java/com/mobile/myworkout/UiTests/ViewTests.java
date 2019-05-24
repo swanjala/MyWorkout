14fc17e99828f7ce1d07cc5325bc2b983b9012f2
swanjala <wanjala.sam.s@gmail.com>
2019-05-24 12:19:51 +0300

    (add): unit test for image view
    - Ensures that the logo is displayed

--- app/src/androidTest/java/com/mobile/myworkout/UiTests/ViewTests.java
+++ app/src/androidTest/java/com/mobile/myworkout/UiTests/ViewTests.java
@@ -1,4 +1,30 @@
 package com.mobile.myworkout.UiTests;
 
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.mobile.myworkout.MainActivity;
+import com.mobile.myworkout.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
 public class ViewTests {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void test_that_image_view_loads_correctly() {
+        onView(withId(R.id.iv_myworkoutlog)).check(matches(isDisplayed()));
+    }
+
 }
