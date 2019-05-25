5b72fdfbc9c3fef535c494af8433529c5c07b86d
swanjala <wanjala.sam.s@gmail.com>
2019-05-25 15:38:22 +0300

    (add): user view model
    - Add a skeleton user view model

--- app/src/main/java/com/mobile/myworkout/viewmodel/UserViewModel.java
+++ app/src/main/java/com/mobile/myworkout/viewmodel/UserViewModel.java
@@ -1,4 +1,46 @@
 package com.mobile.myworkout.viewmodel;
 
-public class UserViewModel {
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+
+import com.mobile.myworkout.model.datamodel.UserModel;
+import com.mobile.myworkout.model.datarepository.AppRepository;
+
+import java.util.List;
+
+public class UserViewModel extends AndroidViewModel {
+
+
+    AppRepository appRepository;
+    List<UserModel> userData;
+
+    public UserViewModel(Application application){
+        super(application);
+
+    }
+
+    public boolean checkEmailRegistration() {
+        /* check if the email exits*/
+        return false;
+    }
+
+    public boolean loginUserWithCredentials(UserModel userModel) {
+
+        return false;
+    }
+    public boolean logOutUser() {
+        /*Nullify shared preference values*/
+
+        return false;
+    }
+
+    public List<UserModel> fetchPersonalData() {
+
+        /*perform an ops here to fetch the data*/
+        return  userData;
+
+    }
+
+
+
 }
