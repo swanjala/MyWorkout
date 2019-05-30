24e8f9ee15969385f9fbcfee8a325095f6a64536
swanjala <wanjala.sam.s@gmail.com>
2019-05-30 21:40:37 +0300

    (add): profile adapter
    - Displays the users details when logged in

--- app/src/main/java/com/mobile/myworkout/view/adapters/ProfileAdapter.java
+++ app/src/main/java/com/mobile/myworkout/view/adapters/ProfileAdapter.java
@@ -1,4 +1,83 @@
 package com.mobile.myworkout.view.adapters;
 
-public class ProfileAdapter {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.mobile.myworkout.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class ProfileAdapter extends
+        RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder> {
+
+    private ArrayList<HashMap<String, String>> mProfileData;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public ProfileAdapter(Context context, ArrayList<HashMap<String, String>> profileData) {
+
+        this.mProfileData = profileData;
+        this.layoutInflater = LayoutInflater.from(context);
+        this.context = context;
+
+
+    }
+
+    @Override
+    public ProfileViewHolder onCreateViewHolder(ViewGroup viewGroup,
+                                                int viewType) {
+        View view = layoutInflater.inflate(R.layout.card_profile_details_layout, viewGroup,
+                false);
+
+        return new ProfileViewHolder(view);
+
+    }
+
+    @Override
+    public void onBindViewHolder(ProfileViewHolder holder, int position) {
+
+        HashMap<String, String> currentData = mProfileData.get(position);
+
+        holder.setData(currentData, position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mProfileData.size();
+    }
+
+
+    class ProfileViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tv_key, tv_value;
+
+        public ProfileViewHolder(View profileView) {
+            super(profileView);
+            tv_key = profileView.findViewById(R.id.tv_key);
+            tv_value = profileView.findViewById(R.id.tv_value);
+
+        }
+
+        public void setData(HashMap<String, String> currentData, final int position) {
+
+
+            if (position == 0) {
+
+                this.tv_key.setText("Email");
+                this.tv_value.setText(currentData.get("Email"));
+
+            } else if (position == 1) {
+                this.tv_key.setText("Gender");
+                this.tv_value.setText(currentData.get("Gender"));
+            }
+
+
+        }
+
+    }
 }
