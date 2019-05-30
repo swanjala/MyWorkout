a0eae6c03b61a1650c84dc0bf8f29e3e3b8ae0a1
swanjala <wanjala.sam.s@gmail.com>
2019-05-30 21:45:36 +0300

    (feat): add profile fragment
    - Implement a collection of hashmaps to
    implement a recycler view of different key types
    
    - Implement a logout functionality

--- app/src/main/java/com/mobile/myworkout/view/fragments/ProfileFragment.java
+++ app/src/main/java/com/mobile/myworkout/view/fragments/ProfileFragment.java
@@ -1,8 +1,14 @@
 package com.mobile.myworkout.view.fragments;
 
+import android.annotation.SuppressLint;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +17,14 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.mobile.myworkout.R;
+import com.mobile.myworkout.model.datamodel.UserModel;
+import com.mobile.myworkout.view.adapters.ProfileAdapter;
 import com.mobile.myworkout.view.navigation.FragmentNavigator;
+import com.mobile.myworkout.viewmodel.UserViewModel;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,6 +40,13 @@ public class ProfileFragment extends Fragment {
     @BindView(R.id.rv_user_details)
     RecyclerView rv_user_details;
 
+    private ProfileAdapter profileAdapter;
+
+    HashMap<String, String> profileInfo =
+            new HashMap<>();
+
+    ArrayList<HashMap<String,String>> profileData = new ArrayList<>();
+
     FragmentNavigator fragmentNavigator = new FragmentNavigator();
 
 
@@ -40,18 +60,44 @@ public class ProfileFragment extends Fragment {
         return view;
 
     }
+    @SuppressLint("NewApi")
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
 
-        bt_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                if (!fragmentNavigator.fragmentLoader(new HomeFragment(),getFragmentManager())){
+        UserViewModel userViewModel = ViewModelProviders
+                .of(this)
+                .get(UserViewModel.class);
+
+        LinearLayoutManager linearLayoutManager =
+                new LinearLayoutManager(getContext());
+
+        rv_user_details.setLayoutManager(linearLayoutManager);
+
+        SharedPreferences preferences = PreferenceManager
+                .getDefaultSharedPreferences(getContext());
+
+
+        profileInfo.put("Email", preferences.getString("email", ""));
+        profileData.add(profileInfo);
+        profileInfo.put("Gender", preferences.getString("gender", ""));
+        profileData.add(profileInfo);
+
+        profileAdapter = new ProfileAdapter(getContext(), profileData);
+
+        rv_user_details.setAdapter(profileAdapter);
+        profileAdapter.notifyDataSetChanged();
+
+        bt_logout.setOnClickListener(v -> {
+
+            if(userViewModel.logOutUser()) {
+
+                if (!fragmentNavigator.fragmentLoader(new HomeFragment(),
+                        getFragmentManager(), null)) {
                     Toast.makeText(getContext(),
-                            "Unable to log you out",Toast.LENGTH_LONG).show();
-                };
+                            "Unable to load the home screen", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
