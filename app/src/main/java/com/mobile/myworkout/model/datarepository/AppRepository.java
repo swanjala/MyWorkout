07d563835ee1c169d713a36caf4a7279ca121524
swanjala <wanjala.sam.s@gmail.com>
2019-05-30 21:36:55 +0300

    (feat): add application repository
    - Manages the network data tasks: login, email and registration

--- app/src/main/java/com/mobile/myworkout/model/datarepository/AppRepository.java
+++ app/src/main/java/com/mobile/myworkout/model/datarepository/AppRepository.java
@@ -1,4 +1,57 @@
 package com.mobile.myworkout.model.datarepository;
 
+import android.app.Application;
+import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
+
+import com.mobile.myworkout.model.datamodel.UserModel;
+import com.mobile.myworkout.model.datarepository.database.MyWorkoutDatabase;
+import com.mobile.myworkout.model.datarepository.database.UserDataAccessObject;
+import com.mobile.myworkout.model.datarepository.datatasks.EmailNetworkTask;
+import com.mobile.myworkout.model.datarepository.datatasks.LoginTask;
+import com.mobile.myworkout.model.datarepository.datatasks.RegistrationTask;
+
+import java.util.List;
+
 public class AppRepository {
+
+    private static MyWorkoutDatabase myWorkoutDatabase;
+    private Application mApplication;
+    private UserDataAccessObject userDataAccessObject;
+    private UserModel mUserModel = new UserModel();
+
+
+    public AppRepository(@NonNull Application application) {
+
+        this.mApplication = application;
+
+        myWorkoutDatabase = MyWorkoutDatabase.getDatabase(mApplication);
+        userDataAccessObject = myWorkoutDatabase.userDataAccessObject();
+    }
+
+    public void verifyEmail(String email, FragmentManager fragmentManager) {
+
+        mUserModel.setEmail(email);
+
+        new EmailNetworkTask(mApplication.getApplicationContext(), fragmentManager).execute(mUserModel);
+    }
+
+
+    public void addNewUser(UserModel userModel) {
+
+        new RegistrationTask(myWorkoutDatabase.userDataAccessObject(),
+                mApplication.getApplicationContext(), userModel).execute();
+
+    }
+
+    public void loginUser(UserModel userModel) {
+        new LoginTask(mApplication.getApplicationContext(), userModel).execute();
+    }
+
+    public List<UserModel> fetchProfileData(String email) {
+
+        return userDataAccessObject.getUserByEmail(email);
+
+    }
+
 }
