707b4d4d54f80a3a7d442206dc3f68dec6c5f434
swanjala <wanjala.sam.s@gmail.com>
2019-05-30 22:21:13 +0300

    (add): view utils
    - Input validations for emails

---
+++ app/src/main/java/com/mobile/myworkout/view/utils/InputValidations.java
@@ -0,0 +1,21 @@
+package com.mobile.myworkout.view.utils;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputValidations {
+
+    public boolean isValidEmail(String email) {
+        boolean isValid = false;
+
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        CharSequence inputStr = email;
+
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(inputStr);
+        if (matcher.matches()) {
+            isValid = true;
+        }
+        return isValid;
+    }
+}
